8ec1c6d3a4a657fbc39ca1f299403b3347e8b425
Axel <backb0x@hotmail.com>
2017-08-09 22:29:49 +0200

    Update exportpgn.py

--- liapi/exportpgn.py
+++ liapi/exportpgn.py
@@ -1,4 +1,4 @@
 import requests
 def pgn_exporter(game_id):
-    url="https://lichess.org/game/export/"+game_id+".pgn"
+    url="https://lichess.org/game/export/{}.pgn".format(game_id)
     return requests.get(url).text
