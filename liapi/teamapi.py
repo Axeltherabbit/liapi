8cf119be1475a63417b49f6faf4bbc40fa597ff1
Axel <backb0x@hotmail.com>
2017-08-11 21:15:53 +0200

    Update teamapi.py

--- liapi/teamapi.py
+++ liapi/teamapi.py
@@ -3,27 +3,30 @@ import requests
 from liapi.userapi import *
 from liapi.page_data import *
 
-def team_user(teamname,us_for_page=10,page_number=1):
 
-        url =  "https://lichess.org/api/user?team={}&nb={}&page={}".format(teamname,us_for_page,page_number)
+def team_user(teamname, us_for_page=10, page_number=1):
+
+        url = "https://lichess.org/api/user?team={}&nb={}&page={}".format(teamname,
+                                                                          us_for_page,
+                                                                          page_number)
 
         try:
-            datajson=requests.get(url).json()
-            datajson=json.dumps(datajson)
+            datajson = requests.get(url).json()
+            datajson = json.dumps(datajson)
         except:
             print("ERROR: team_user except")
             return
 
-        j=json.loads(datajson)
+        j = json.loads(datajson)
 
-        us_list=page_data(j["paginator"])
+        us_list = page_data(j["paginator"])
 
-        sz=len(j["paginator"]['currentPageResults'])
-        count=0
+        sz = len(j["paginator"]['currentPageResults'])
+        cnt = 0
 
-        while (sz>count):
-            tmp=user(json.dumps(j["paginator"]['currentPageResults'][count]),1)
-            us_list[tmp.id]=tmp
-            count+=1
+        while (sz > cnt):
+            tmp = user(json.dumps(j["paginator"]['currentPageResults'][cnt]), 1)
+            us_list[tmp.id] = tmp
+            cnt += 1
 
         return us_list
