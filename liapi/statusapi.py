1dcfc674c617673364419834de354ef68fa91f7b
Axel <backb0x@hotmail.com>
2017-08-11 21:12:41 +0200

    update to pep8

--- liapi/statusapi.py
+++ liapi/statusapi.py
@@ -1,22 +1,24 @@
 import json
 import requests
+
+
 class jdata():
-    def __init__(self,j):
-        content=["id","name","online","playing"]
+    def __init__(self, j):
+        content = ["id", "name", "online", "playing"]
         for i in content:
-            self.__dict__[i]=j[i]
+            self.__dict__[i] = j[i]
 
 
 def userstats(players):
-    url="https://lichess.org/api/users/status?ids="+players
+    url = "https://lichess.org/api/users/status?ids={}".format(players)
     try:
-        datajson=json.dumps(requests.get(url).json())
+        datajson = json.dumps(requests.get(url).json())
     except:
         print("Error datajson except")
         return 0
 
-    j=json.loads(datajson)
-    us_list={}
+    j = json.loads(datajson)
+    us_list = {}
     for i in j:
-        us_list[i["id"]]=jdata(i)
+        us_list[i["id"]] = jdata(i)
     return us_list
