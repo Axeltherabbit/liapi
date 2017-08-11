9c399055dfcedcff99877df338731088dfe261a2
Axel <backb0x@hotmail.com>
2017-08-11 22:02:13 +0200

    update to pep8

--- liapi/tournamentapi.py
+++ liapi/tournamentapi.py
@@ -1,65 +1,64 @@
 import requests
 import json
 
+
 class tournaments():
-    def __init__(self,j):
-        content=["id",
-                 "createdBy",
-                 "system",
-                 "minutes",
-                 "position",
-                 "rated",
-                 "fullName",
-                 "nbPlayers",
-                 "private",
-                 "secondsToStart",
-                 "startsAt",
-                 "finishesAt",
-                 "status",
-                 "schedule",
-                 "winner",
-                 "conditions"
-                 ]
+    def __init__(self, j):
+        content = ["id",
+                   "createdBy",
+                   "system",
+                   "minutes",
+                   "position",
+                   "rated",
+                   "fullName",
+                   "nbPlayers",
+                   "private",
+                   "secondsToStart",
+                   "startsAt",
+                   "finishesAt",
+                   "status",
+                   "schedule",
+                   "winner",
+                   "conditions"]
 
         for i in content:
             try:
-                self.__dict__[i]=j[i]
+                self.__dict__[i] = j[i]
             except:
-                self.__dict__[i]=None
+                self.__dict__[i] = None
 
-        content=["limit","increment"]
+        content = ["limit", "increment"]
         for i in content:
             try:
-                self.__dict__["clock_"+i]=j["clock"][i]
+                self.__dict__["clock_"+i] = j["clock"][i]
             except:
-                self.__dict__["clock_"+i]=None
+                self.__dict__["clock_"+i] = None
 
-        content=["key","short","name"]
+        content = ["key", "short", "name"]
         for i in content:
             try:
-                self.__dict__["variant_"+i]=j["variant"][i]
+                self.__dict__["variant_"+i] = j["variant"][i]
             except:
-                self.__dict__["variant_"+i]=None
+                self.__dict__["variant_"+i] = None
 
-        content=["icon","name","position"]
+        content = ["icon", "name", "position"]
         for i in content:
             try:
-                self.__dict__["perf_"+i]=j["perf"][i]
+                self.__dict__["perf_"+i] = j["perf"][i]
             except:
-                self.__dict__["perf_"+i]=None
-
-
-
+                self.__dict__["perf_"+i] = None
 
 
 def get_tournaments(state):
-    if state==("created" or "started" or "finished"):
-        datajson=json.dumps(requests.get("https://lichess.org/api/tournament").json())
-        j=json.loads(datajson)
+    if state == ("created" or "started" or "finished"):
+        url = "https://lichess.org/api/tournament"
+        datajson = json.dumps(requests.get(url).json())
 
-        tournament_list={}
+        j = json.loads(datajson)
+
+        tournament_list = {}
         for i in j[state]:
-            tournament_list[i["id"]]=tournaments(i)
+            tournament_list[i["id"]] = tournaments(i)
         return tournament_list
 
     else:
