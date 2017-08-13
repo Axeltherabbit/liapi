9c38333b3c4d589f036ee095bc5c3244a281e244
Axel <backb0x@hotmail.com>
2017-08-14 00:01:42 +0200

    fix nofound() users

--- liapi/statusapi.py
+++ liapi/statusapi.py
@@ -1,12 +1,15 @@
 import json
 import requests
+from liapi.found_class import *
 
 
 class jdata():
     def __init__(self, j):
+        self.Found=1
         content = ["id", "name", "online", "playing"]
         for i in content:
             self.__dict__[i] = j[i]
+
 
 
 def userstats(players):
@@ -21,4 +24,10 @@ def userstats(players):
     us_list = {}
     for i in j:
         us_list[i["id"]] = jdata(i)
+
+    us_names = players.split(",")
+    nofound_games = set(us_names) - us_list.keys()
+    for i in nofound_games:
+        us_list[i] = nofound()
+
     return us_list
